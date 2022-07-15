6451c312861da0af70c3adf97e3680921f4d89cd
yovelal <100790447+yovelal@users.noreply.github.com>
2022-07-15 12:51:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include "funcs.c"
 
 
@@ -17,12 +18,14 @@ int main (int argc, char **argv) {
 		info = parse(cmdLine); 
 		
 		if(info->command && strcmp(info->command[0],"exit")==0){
+			freeInfo(info);
 			exit(1);
 		}
 		else if(info->command && strcmp(info->command[0],"cd")==0){
 			//if the directory not found print error 
 			if(chdir(info->command[1])==-1){
 				printf("%s NOT FOUND\n", info->command[1]);
+				freeInfo(info);
 			}
 			continue;
     	}
@@ -34,7 +37,6 @@ int main (int argc, char **argv) {
 			/* child code */
 			executeCommand(info); //calls  execvp
 			freeInfo(info);
-			
 		}
 		else 
 		{ 
